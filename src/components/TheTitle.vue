a72002bcb894639ee23241a5134dfc9b37494d3d
joaoHenrique001 <joaogamesbrbrazilhotmail9@gmail.com>
2025-03-31 11:20:55 +0200

    ejercicio 2

---
+++ src/components/TheTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2 class="title">Título estático</h2>
+  </template>
+  
+  <style scoped>
+  .title {
+    color: blue;
+    font-size: 24px;
+    text-align: center;
+  }
+  </style>
+  
