b3abc7a0042d80280c9a1a6b84e0da28663e1fee
joaoHenrique001 <joaogamesbrbrazilhotmail9@gmail.com>
2025-03-31 11:37:43 +0200

    Revert "ejercicio 2"
    This reverts commit fd3cf1f8adef176e39acda4920c7dc242be8f2c4.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <TheTitle />
+  <div>
+    <TheTitle propTitulo="Mi título dinámico" />
+  </div>
 </template>
 
 <script>
-import TheTitle from './components/TheTitle.vue'
+import TheTitle from './components/TheTitle.vue';
 
 export default {
-  name: 'App',
   components: {
     TheTitle
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/TheTitle.vue
+++ src/components/TheTitle.vue
@@ -1,6 +1,18 @@
 <template>
-    <h2 class="title">Título estático</h2>
+    <h2 class="title">{{ propTitulo }}</h2>
   </template>
+  
+  <script>
+  export default {
+    // Declaracion del prop llamado
+    props: {
+      propTitulo: {
+        type: String,
+        required: true // Indica que este prop es obligatorio
+      }
+    }
+  }
+  </script>
   
   <style scoped>
   .title {
